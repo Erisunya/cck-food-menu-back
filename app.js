c0f50fa8d9d2b95ccc38d18b1b965863958ee7b9
Erisunya <ojjstuff@gmail.com>
2023-08-31 17:24:47 +0800

    Fix bug where app.js cannot access .env values

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv").config;
+require("dotenv").config();
 
 mongoose.set("strictQuery", false);
 const mongoDB = process.env.MONGODB_URI;
-main().catch((err) => console.log(err));
 const main = async () => {
   await mongoose.connect(mongoDB);
   console.log("Connected to MongoDB");
 };
+main().catch((err) => console.log(err));
 
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => {
   console.log(`Server started, listening on port ${PORT}!`);
 });
